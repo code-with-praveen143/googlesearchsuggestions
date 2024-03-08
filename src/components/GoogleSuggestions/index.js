cdfc7ff1fb9c826406c7282327f18c16250b0571
Talari Praveen Kumar <116370782+code-with-praveen143@users.noreply.github.com>
2024-03-08 20:34:05 +0530

    Update index.js

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,39 +1,45 @@
-// Write your code here
 import {Component} from 'react'
 import SuggestionItem from '../SuggestionItem'
 import './index.css'
 
 class GoogleSuggestions extends Component {
-  state = {searchInput: ''}
+  state = {
+    searchInput: '',
+  }
 
   updateSearchInput = value => {
     this.setState({searchInput: value})
   }
 
   onChangeSearchInput = event => {
-    this.setState({searchInput: event.target.value})
+    this.setState({
+      searchInput: event.target.value,
+    })
   }
 
   render() {
-    const {suggestionsList} = this.props
     const {searchInput} = this.state
-    const searchResults = suggestionsList.filter(eachItem =>
-      eachItem.suggestion.toLowerCase().includes(searchInput.toLowerCase()),
+    const {suggestionsList} = this.props
+    const searchResults = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
     )
+
     return (
-      <div className="bg-container">
-        <div className="card-container">
+      <div className="app-container">
+        <div className="google-suggestions-container">
           <img
             src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
             alt="google logo"
-            className="google-img"
+            className="google-logo"
           />
-          <div className="search-input-suggestion-container">
+          <div className="search-input-suggestions-container">
             <div className="search-input-container">
               <img
                 alt="search icon"
+                className="search-icon"
                 src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
-                className="search-img"
               />
               <input
                 type="search"
@@ -43,16 +49,16 @@ class GoogleSuggestions extends Component {
                 value={searchInput}
               />
             </div>
+            <ul className="suggestions-list">
+              {searchResults.map(eachSuggestion => (
+                <SuggestionItem
+                  key={eachSuggestion.id}
+                  suggestionDetails={eachSuggestion}
+                  updateSearchInput={this.updateSearchInput}
+                />
+              ))}
+            </ul>
           </div>
-          <ul className="suggestions-list">
-            {searchResults.map(eachItem => (
-              <SuggestionItem
-                key={eachItem.id}
-                suggestionDetails={eachItem}
-                updateSearchInput={this.updateSearchInput}
-              />
-            ))}
-          </ul>
         </div>
       </div>
     )
